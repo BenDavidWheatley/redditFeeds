7ec1dc342eea4e3d20eb693b0d714cfccf24ab8a
Ben Wheatley <info@wheatleystudios.com>
2025-01-28 21:50:23 +0000

    Footer
    Simple footer added, excluding any social links.

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import Styles from './footer.module.css'
+import Styles from './footer.module.css';
 
 function Footer() {
-    return  (
-        <div data-testId='footer' className={Styles.footerContainer}>
-           
+    return (
+        <div data-testId="footer" className={Styles.footerContainer}>
+            <div className={Styles.footerContent}>
+         
+                <div className={Styles.version}>
+                    <span>Version 1.0.0</span>
+                </div>
+
+                <div className={Styles.copyright}>
+                    <span>© 2025 Readit Reddit. All rights reserved.</span>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Footer;
